eb5d257033ba98f9b6148fd1bc7a963764c6f599
程兰兰 <chenglanlan@192.168.0.104>
2019-06-16 14:31:42 +0800

    m

---
+++ 星河保理/js/public.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', function (){
+	var shuping = 'onorientationchange' in window ? 'orientationchange' : 'resize';
+	var timer = null;
+
+	//设置字体
+	function setFontSize(){
+		var w = window.innerWidth;
+		if(w<640){
+		  document.documentElement.style.fontSize = 100*w/640 + 'px';
+	    }else{
+		  document.documentElement.style.fontSize = 100 + 'px';
+	    }
+	}
+	setFontSize();
+	window.addEventListener(shuping, function (){
+		clearTimeout(timer);
+		timer = setTimeout(setFontSize, 300);
+	}, false);
+}, false);
+
